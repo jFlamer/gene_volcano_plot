2f2f37de0be6fe3b4f44b857426667d0bd1e7d0b
Jagoda Flejmer <jflejmer@student.agh.edu.pl>
2025-04-02 17:20:44 +0200

    Uploaded excel data, created function to read them, created basic plot using log10 [app.py] and display of png picture [html.index]

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-from flask import Flask, render_template
+import io
+
+import matplotlib
+import numpy as np
+import pandas as pd
+from flask import Flask, render_template, send_file
+from matplotlib import pyplot as plt
+
+matplotlib.use('Agg')
 
 app = Flask(__name__)
 
+def read_data(file_path, sheet_name):
+    df = pd.read_excel(file_path, sheet_name=sheet_name, skiprows=2)
+    df.columns = df.columns.str.strip()
+    return df
+
+@app.route('/plot')
+def create_plot():
+    df = read_data(r"data/NIHMS1635539-supplement-1635539_Sup_tab_4.xlsx", "S4B limma results")
+    plt.figure(figsize=(20,12))
+    plt.scatter(df['logFC'], -np.log10(df['adj.P.Val']), c = -np.log10(df['adj.P.Val']), cmap='gist_rainbow', alpha=0.7)
+
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    plt.close()
+    return send_file(buffer, mimetype='image/png')
 @app.route('/')
 def home():
     return render_template("index.html")
